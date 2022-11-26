d118adeb0e81a62fd386a0363e37f7e4b981d05b
peter <peter@taswira.ai>
2022-11-26 12:00:14 +0300

    Initial commit

--- run.py
+++ run.py
@@ -8,14 +8,7 @@ person_pickle = pickle.load(picklefile)
 t=person_pickle('john', 10)
 print(t.myfunc())
 
-# @app.route('/')
-# def index():
-#    return render_template('index.html')
-# if __name__ == '__main__':
-#     print('File okay')
-#     picklefile = open('test_pickle', 'rb')
-#     #unpickle the dataframe
-#     laptop1 = pickle.load(picklefile)
-#     t=laptop1('john', 10)
-#     print(t.myfunc())  
+
+if __name__ == '__main__':
+    print('File okay')
 	
